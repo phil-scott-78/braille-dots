b20a5201ae57c397320d5889987b35174a494793
Phil Scott <phil.scott@gmail.com>
2021-02-06 00:06:40 -0500

    a few more tests

--- tests/BrailleDots.Tests/UnitTest1.cs
+++ tests/BrailleDots.Tests/UnitTest1.cs
@@ -1,11 +1,29 @@
 using System;
 using System.Collections;
+using System.Runtime.InteropServices;
 using Xunit;
 
 namespace BrailleDots.Tests
 {
     public class UnitTest1
     {
+        [Theory]
+        [InlineData("1", '⠁')]
+        [InlineData("2", '⠂')]
+        [InlineData("3", '⠄')]
+        [InlineData("4", '⠈')]
+        [InlineData("5", '⠐')]
+        [InlineData("6", '⠠')]
+        [InlineData("7", '⡀')]
+        [InlineData("8", '⢀')]
+        [InlineData("235", '⠖')]
+        [InlineData("12456", '⠻')]
+        [InlineData("12568", '⢳')]
+        public void Can_convert_characters(string pattern, char expectedDots)
+        {
+            Assert.Equal(expectedDots, DotPattern.Get(pattern));
+        }
+        
         [Fact]
         public void Test1()
         {
